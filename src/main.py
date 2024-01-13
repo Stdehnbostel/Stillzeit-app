50378fe78283ce9d5405edd3400920d4309bdf5d
Stefan Wagner <112750321+Stdehnbostel@users.noreply.github.com>
2024-01-13 16:02:02 +0100

    improve the code's raedability

--- src/main.py
+++ src/main.py
@@ -14,25 +14,10 @@ from datetime import datetime as DateTime
 
 # Gui
 Builder.load_string("""
-<Startseite>
-    GridLayout:
-        cols: 1
-        Button:
-            text: "Neue Stillzeit erfassen"
-	        on_press: 
-                root.manager.current = "messen"
-                root.manager.transition.direction = "right"
 
-        GridLayout:
-            cols:2
-            Button:
-                text: "Letztes Stillen"
-            Button:
-                text: "Protokoll anzeigen"
-
-<Zeitmessen>
+<MeasureFeedingTime>
 	start: btn
-    kommentar: Kom.text	
+    comment: Kom.text	
 
 	GridLayout:
 		cols: 1
@@ -49,14 +34,14 @@ Builder.load_string("""
 			Label:
 				text: "Beginn: "
 			Label:
-				text: root.anfang
+				text: root.startTimeLabel
 				id: anf
 			Label:
 				text: "Ende: "
 			Label:
-				text: root.ende
+				text: root.endTimeLabel
 		Label:
-			text: root.stilldauer
+			text: root.durationLabel
 			size_hint: (1.,0.3)
 		GridLayout:
 			cols: 2 
@@ -69,16 +54,16 @@ Builder.load_string("""
 			    text: "letztes Stillen"
 			    size_hint: (1.,0.3)
 			    on_press:
-				    root.manager.current = "zeiten"
+				    root.manager.current = "times"
 				    root.manager.transition.direction = "right"
 		    Button:
 			    text: "Protokoll anzeigen"
 			    size_hint: (1.,0.3)
 			    on_press:
-				    root.manager.current = "zeigen"
+				    root.manager.current = "show"
 				    root.manager.transition.direction = "left"
-				    root.lesen()
-<Protokoll>
+				    root.readProtocol()
+<Protocol>
     on_enter:
         root.start()
 	GridLayout:
@@ -86,213 +71,199 @@ Builder.load_string("""
         Button:
             text: "ältere Einträge"
             on_press:
-                root.Aelter()
+                root.getOlderEntries()
         Label:
-            text: root.anzeige1
+            text: root.label1
         Label:
-            text: root.anzeige2
+            text: root.label2
         Label:
-            text: root.anzeige3
+            text: root.label3
         Label:
-            text: root.anzeige4
+            text: root.label4
         Label:
-            text: root.anzeige5
+            text: root.label5
         Label:
-            text: root.anzeige6
+            text: root.label6
         Label:
-            text: root.anzeige7
+            text: root.label7
         Label:
-            text: root.anzeige8
+            text: root.label8
         Label:
-            text: root.anzeige9
+            text: root.label9
         Label:
-            text: root.anzeige10
+            text: root.label10
         Button:
             text: "neuere Einträge"
             on_press:
-                root.Neuer()
+                root.getNewerEntries()
 	    Button:
 	        text: "Zurück"
 	        on_press:
-    	    	root.manager.current = "messen"
+    	    	root.manager.current = "measure"
     	    	root.manager.transition.direction = "right"
-<Zeiten>
-    on_enter: root.lesen()
+<FeedingTimes>
+    on_enter: root.read()
     GridLayout:
         cols: 1
         Label:
-            text: root.anzeigediff
+            text: root.labelTimeSince
         Label:
-            text: root.anzeigezeit
+            text: root.labelTime
         Button:
             text: "Zurück"
             size_hint: (1.,0.2)
             on_press:
-                root.manager.current = "messen"
+                root.manager.current = "measure"
                 root.manager.transition.direction = "left"
 """)
 
-class Zeitmessen(Screen):
+class MeasureFeedingTime(Screen):
     start = ObjectProperty()
-    anfang = StringProperty()
-    ende = StringProperty()
-    stilldauer = StringProperty()
-    kommentar = StringProperty()
+    startTimeLabel = StringProperty()
+    endTimeLabel = StringProperty()
+    durationLabel = StringProperty()
+    comment = StringProperty()
 
-    messen = False
-    zeit1 = time.time()
-    zeit2 = time.time()
-    zeitstempel = DateTime.now()
+    measure = False
+    time1 = time.time()
+    time2 = time.time()
+    timeStamp = DateTime.now()
 
     def zeiterfassen(self):
-        self.zeitstempel = DateTime.now()
+        self.timeStamp = DateTime.now()
 
-        if self.messen == False:
-            self.zeit1 = time.time()
-            self.messen = True
-            self.beginn()
-        elif self.messen == True:
-            self.zeit2 = time.time()
-            self.messen = False
-            dauer = self.zeit2 - self.zeit1
-            dauermin = dauer  / 60
-            dauermin = int(dauermin)
-            dauersek = dauer - dauermin
-            self.stilldauer = "Stillzeit: "+str(dauermin)+" Minuten"
-            self.beendet()
+        if self.measure == False:
+            self.time1 = time.time()
+            self.measure = True
+            self.beginMeasurement()
+        elif self.measure == True:
+            self.time2 = time.time()
+            self.measure = False
+            duratin = self.time2 - self.time1
+            durationInMinutes = int(duratin / 60)
+            self.durationLabel = "Stillzeit: "+str(durationInMinutes)+" Minuten"
+            self.endMeasurement()
 
-    def ZuletztGestillt(self):
-        with open('LetztesStillen.txt', 'w') as letztes_stillen:
-            letztes_stillen.write(str(self.zeit2)+"\n"+str(self.zeitstempel.strftime("%H:%M")))
+    def saveLastFeedingTime(self):
+        with open('lastFeeding.txt', 'w') as f:
+            f.write(str(self.time2)+"\n"+str(self.timeStamp.strftime("%H:%M")))
 
-    def beginn(self):
-        self.anfang = self.zeitstempel.strftime("%H:%M")
-        self.ende = ""
-        with open ('Protokoll.txt', 'a') as protokoll:
-            protokoll.write("\n"+str(self.zeitstempel.strftime("%D: %H:%M")))
-        self.ZuletztGestillt()
+    def beginMeasurement(self):
+        self.startTimeLabel= self.timeStamp.strftime("%H:%M")
+        self.endTimeLabel = ""
+        with open ('Protokoll.txt', 'a') as protocol:
+            protocol.write("\n"+str(self.timeStamp.strftime("%D: %H:%M")))
+        self.saveLastFeedingTime()
 
-    def beendet(self):
-        self.ende = self.zeitstempel.strftime("%H:%M")
-        dauer = self.zeit2 - self.zeit1
-        dauermin = dauer  / 60
-        dauermin = int(dauermin)
-        with open('Protokoll.txt', 'a') as protokoll:
-            protokoll.write(str(self.zeitstempel.strftime(" bis %H:%M"))+"\n")
-            protokoll.write(str(dauermin)+" Minuten\n")
-        if self.kommentar != "":
-            with open('Protokoll.txt', 'a') as protokoll:
-                protokoll.write(self.kommentar+"\n")
-            self.kommentar = ""
-        self.ZuletztGestillt()
+    def endMeasurement(self):
+        self.endTimeLabel = self.timeStamp.strftime("%H:%M")
+        duration = self.time2 - self.time1
+        durationInMinutes = int(duration / 60)
+        with open('Protokoll.txt', 'a') as protocol:
+            protocol.write(str(self.timeStamp.strftime(" bis %H:%M")) + "\n")
+            protocol.write(str(durationInMinutes) + " Minuten\n")
+            if self.comment != "":
+                protocol.write(self.comment + "\n")
+                self.comment= ""
+        self.saveLastFeedingTime()
 
-    def lesen(self):
-        return Protokoll().lesen()
+    def readProtocol(self):
+        return Protocol().readProtocol()
 
-class Startseite(Screen, GridLayout):
-    pass
-
-class Protokoll(Screen, GridLayout): 
+class Protocol(Screen, GridLayout): 
         
-    anzeige1 = StringProperty()
-    anzeige2 = StringProperty()
-    anzeige3 = StringProperty()
-    anzeige4 = StringProperty()
-    anzeige5 = StringProperty()
-    anzeige6 = StringProperty()
-    anzeige7 = StringProperty()
-    anzeige8 = StringProperty()
-    anzeige9 = StringProperty()
-    anzeige10 = StringProperty()
+    label1 = StringProperty()
+    label2 = StringProperty()
+    label3 = StringProperty()
+    label4 = StringProperty()
+    label5 = StringProperty()
+    label6 = StringProperty()
+    label7 = StringProperty()
+    label8 = StringProperty()
+    label9 = StringProperty()
+    label10 = StringProperty()
+    
     limit = 0
-    eintraege = []
-    zeige = 0
+    etries = []
+    offset = 0
 
     def start(self):
+        self.limit = self.readProtocol()
+        self.offset = self.limit
+        self.updateLabel()
 
-        self.zeige = self.lesen()
-        self.Label()
-
-    def lesen(self):
-        i = 0
-        a = 11
-        self.eintraege = []
+    def readProtocol(self):
+        lines = 0
+        self.entries = []
 
         try:
-            with open('Protokoll.txt', 'r') as protokoll:
-                for eintrag in protokoll:
-                    i = i + 1
-                    self.eintraege.append(eintrag)
+            with open('Protokoll.txt', 'r', encoding="utf-8") as protocol:
+                for entry in protocol:
+                    lines = lines + 1
+                    self.entries.append(entry)
         except FileNotFoundError:
-            eintrag = "Es ist noch kein Protokoll vorhanden"
-            self.eintraege.append(eintrag)
+            entry = "Es ist noch kein Protokoll vorhanden"
+            self.entries.append(entry)
 
-        self.limit = i
+        i = 11 - lines
+        while i > 0:
+            self.entries.append("")
+            i = i-1
 
-        a = a - i
+        if lines < 10:
+            lines = 10
 
-        while a > 0:
-            self.eintraege.append("")
-            a = a-1
+        return lines
 
-        if i < 10:
-            i = 10
+    def getOlderEntries(self):
+        self.offset = self.offset - 9
+        if self.offset < 10:
+            self.offset = 10
+        self.updateLabel()
 
-        return i
-
-    def Aelter(self):
-        self.lesen()
-        self.zeige = self.zeige - 9
-        if self.zeige < 10:
-            self.zeige = 10
-        self.Label()
-
-    def Neuer(self):
-        self.lesen()
-        if self.zeige + 9 < self.limit:
-            self.zeige = self.zeige + 9
+    def getNewerEntries(self):
+        if self.offset + 9 < self.limit:
+            self.offset = self.offset + 9
         else:
-            self.zeige = self.limit
+            self.offset = self.limit
+        self.updateLabel()
 
-        self.Label()
+    def updateLabel(self):
+        self.label1 = self.entries[self.offset - 10]
+        self.label2 = self.entries[self.offset - 9]
+        self.label3 = self.entries[self.offset - 8]
+        self.label4 = self.entries[self.offset - 7]
+        self.label5 = self.entries[self.offset - 6]
+        self.label6 = self.entries[self.offset - 5]
+        self.label7 = self.entries[self.offset - 4]
+        self.label8 = self.entries[self.offset - 3]
+        self.label9 = self.entries[self.offset - 2]
+        self.label10 = self.entries[self.offset - 1]
 
-    def Label(self):
-        self.anzeige1 = self.eintraege[self.zeige-10]
-        self.anzeige2 = self.eintraege[self.zeige-9]
-        self.anzeige3 = self.eintraege[self.zeige-8]
-        self.anzeige4 = self.eintraege[self.zeige-7]
-        self.anzeige5 = self.eintraege[self.zeige-6]
-        self.anzeige6 = self.eintraege[self.zeige-5]
-        self.anzeige7 = self.eintraege[self.zeige-4]
-        self.anzeige8 = self.eintraege[self.zeige-3]
-        self.anzeige9 = self.eintraege[self.zeige-2]
-        self.anzeige10 = self.eintraege[self.zeige-1]
+class FeedingTimes(Screen):
+    labelTimeSince = StringProperty()
+    labelTime = StringProperty()
 
-class Zeiten(Screen):
-
-    anzeigediff = StringProperty()
-    anzeigezeit = StringProperty()
-
-    def lesen(self):
+    def read(self):
         try:
-            with open('LetztesStillen.txt', 'r') as letztes_stillen:
-                zeiten = letztes_stillen.readlines()
+            with open('lastFeeding.txt', 'r') as f:
+                times = f.readlines()
         except FileNotFoundError:
-            zeiten = ["0", "Fehler, Datei nicht gefunden"]
-        zeitanzeige = zeiten[1]
-        zeit1 = float(zeiten[0])
-        zeit2 = time.time()
-        zeitseitStillen = zeit2 - zeit1
-        zeitseitStillenh = int(zeitseitStillen / 3600)
-        zeitseitStillenmin = int(zeitseitStillen / 60)
-        zeitseitStillenmin = zeitseitStillenmin - zeitseitStillenh * 60
-        self.anzeigediff = "Zuletzt gestillt vor "+str(zeitseitStillenh)+":"+"{:02}".format(zeitseitStillenmin)+" Stunden"
-        self.anzeigezeit = "Um "+zeitanzeige+" Uhr"
+            times = ["0", "0"]
+        showTime = times[1]
+        timeOfFeeding = float(times[0])
+        currentTime = time.time()
+        secondsSince = currentTime - timeOfFeeding
+        hoursSince = int(secondsSince / 3600)
+        minutesSince = int(secondsSince / 60)
+        minutesSince = minutesSince - hoursSince * 60
+        self.labelTimeSince = "Zuletzt gestillt vor " + str(hoursSince) + ":" + "{:02}".format(minutesSince) + " Stunden"
+        self.labelTime = "Um " + showTime + " Uhr"
 
 SM = ScreenManager()
-SM.add_widget(Zeitmessen(name='messen'))
-SM.add_widget(Protokoll(name='zeigen'))
-SM.add_widget(Zeiten(name='zeiten'))
+SM.add_widget(MeasureFeedingTime(name='measure'))
+SM.add_widget(Protocol(name='show'))
+SM.add_widget(FeedingTimes(name='times'))
 
 class Stillzeit(App):
     def build(self):
